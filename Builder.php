46c3eb0ae55523d1832c3fce149167ee0dda6429
Anton Tuyakhov <atuyakhov@gmail.com>
2015-09-27 19:59:44 +0300

    dirty fix

--- Builder.php
+++ Builder.php
@@ -37,6 +37,74 @@ class Builder extends  Widget
             targets: $('{$this->containerSelector}'),
             save: " . new JsExpression($this->saveCallback) . ",
             startingModel: {$jsonModel}
-        });");
+        });
+        myForm.setModelValue = function( id, type, val ){
+
+          // Some paths are namespaced
+          var path = false;
+          if( type.indexOf('.') > -1 ){
+            path = type.split('.');
+            type = path[0];
+          }
+
+          if( this._model[id] === undefined ){
+            throw new Error('Model has no entry for ' + id);
+          }
+
+          if( this._model[id].type === undefined ){
+            throw new Error('Invalid schema field ' + type + ' for model ' + id);
+          }
+
+          var fieldType = this.getFieldTypeByName( this._model[id].type );
+
+          // Special handling for choice
+          if( type === 'choices' ){
+
+            var index = parseInt(path[1],10);
+
+            // verify field is in schema
+            if( this._model[id][type][index][path[2]] === undefined  ){
+              throw new Error('Invalid choice schema field ' + path[2] + ' for model ' + id);
+            }
+
+            // set value
+            this._model[id][type][index][path[2]] = val;
+
+            return;
+
+          }
+
+          this._model[id][type] = val;
+
+        };
+        myForm.appendFieldToFormElementEditor = function( frmb_group, field, parentModel, existingModel, index ){
+
+          // load additional details template
+          if( field.template !== undefined ){
+
+            // choices
+            if( parentModel.choices !== undefined ){
+
+              // Create a new model
+              if( typeof existingModel !== 'object' ){
+                existingModel = $.extend(true,{},field.choiceSchema);
+                parentModel.choices.push( existingModel);
+              }
+
+              var bodyObj = {
+                fbid: parentModel.fbid,
+                model: existingModel
+              };
+
+              // new index
+              index = (typeof index === 'number') ? index : (parentModel.choices.length - 1);
+              bodyObj.fbid += '_choices.'+index;
+
+              dust.render(field.template, bodyObj, function(err, out){
+                frmb_group.find('.frmb-choices').append( out );
+              });
+            }
+          }
+        };");
     }
 }
